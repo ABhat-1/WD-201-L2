10ad0819585ea1c740824b1bcbf2f468c9e9829b
ABhat-1 <73524692+ABhat-1@users.noreply.github.com>
2021-10-01 15:02:49 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -17,17 +17,31 @@ domain = get_command_line_argument
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
+def parse_dns(dns_raw)
+  dns_records_data = dns_raw.select { |line| line[0] != "#" && line[0] != "\n"}
+  dns_hash = {}
+  dns_records_data.each{ |line|
+    line_split= line.split(", ").map(&:strip)
+    dns_hash[line_split[1]] = {type: line_split[0], target: line_split[2]}
+  }
+  return dns_hash
+end
 
-
-
-
-
-# ..
-# ..
-# FILL YOUR CODE HERE
-# ..
-# ..
-
+def resolve(dns_records_hash, lookup_chain, domain)
+  value = dns_records_hash[domain]
+  if (!value)
+    lookup_chain<< "Error: Value not found for "+ domain
+  elsif value[:type] == "CNAME"
+    lookup_chain.push(value[:target])
+    resolve(dns_records_hash, lookup_chain, value[:target])
+  elsif value[:type] == "A"
+    lookup_chain.push(value[:target])
+    return lookup_chain
+  else
+    lookup_chain << "Invalid type for "+ domain
+    return
+  end
+end
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
